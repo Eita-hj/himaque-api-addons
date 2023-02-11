318a10ae4d9dc7c49116188a531a7af01b781f3c
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-02-11 15:50:12 +0900

    SpamCheckTypeを追加

---
+++ packages/SpamChecker/src/SpamCheckType.js
@@ -0,0 +1,18 @@
+module.exports = {
+  0: "AreaMessageCreate",
+  1: "GuildMessageCreate",
+  2: "DirectMessageCreate",
+  "A": "AreaMessageCreate",
+  "G": "GuildMessageCreate",
+  "D": "DirectMessageCreate",
+  "Area": "AreaMessageCreate",
+  "Guild": "GuildMessageCreate",
+  "Direct": "DirectMessageCreate",
+  "AreaMessage": "AreaMessageCreate",
+  "GuildMessage": "GuildMessageCreate",
+  "DirectMessage": "DirectMessageCreate",
+  "DM": "DirectMessageCreate",
+  "AreaMessageCreate": "AreaMessageCreate",
+  "GuildMessageCreate": "GuildMessageCreate",
+  "DirectMessageCreate": "DirectMessageCreate"
+}
